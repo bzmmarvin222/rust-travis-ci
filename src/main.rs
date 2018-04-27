b479f0270aee758235fdbc0f4727743aad0693bd
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-04-27 10:01:20 +0200

    added sample test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_three_returns_3() {
+        assert_eq!(get_three(), 3);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("this program does not serve any functionality, it's just about the tests.");
+}
+
+fn get_three() -> i32 {
+    3
 }
